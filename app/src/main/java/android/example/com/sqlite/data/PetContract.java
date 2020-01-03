11f8938db9f2541dea050baabbcc00c9000fd971
ioanz <ioan.zicu94@gmail.com>
2020-01-03 09:56:06 +0100

    add MIME getType() method

--- app/src/main/java/android/example/com/sqlite/data/PetContract.java
+++ app/src/main/java/android/example/com/sqlite/data/PetContract.java
@@ -37,7 +37,7 @@ public final class PetContract {
          */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
-        public static final String CONTENT_TYPE =
+        public static final String CONTENT_LIST_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
